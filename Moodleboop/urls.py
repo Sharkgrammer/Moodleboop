ea5d3d9bb823ce296f904328ec0c281ee895d86f
Sharkie <gillskk@gmail.com>
2018-10-03 16:18:38 +0100

    Updated project layout

--- Moodleboop/urls.py
+++ Moodleboop/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('main.urls')),
-	path('main/', include('main.urls')),
+	path('jobs/', include('jobs.urls')),
+	path('interviews/', include('interviews.urls')),
     path('admin/', admin.site.urls),
 ]
