ac16863971bb45fa83b624f7c0677195acfab76f
Sharkie <gillskk@gmail.com>
2018-10-03 18:49:55 +0100

    Css update

--- main/templatetags/link_tags.py
+++ main/templatetags/link_tags.py
@@ -6,7 +6,7 @@ def get_links(LinkFull, r):
     from lxml import html
     import requests
     data = ""
-    Link = LinkFull.split("href='")[1].replace("'>Submit link</a></b></h3>", "")
+    Link = split_link(LinkFull, 1)
     #return Link
     page2 = r.get(Link)
     tree2 = html.fromstring(page2.content)
