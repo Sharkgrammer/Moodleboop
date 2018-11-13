4e177120bdc251483826f3e65625c8514e22a768
Sharkie <gillskk@gmail.com>
2018-11-13 15:07:33 +0000

    Minor fix to Nonetype error in Jobs page

--- main/templatetags/link_tags.py
+++ main/templatetags/link_tags.py
@@ -12,7 +12,7 @@ def get_links(LinkFull, r):
     tree2 = html.fromstring(page2.content)
     Links = tree2.xpath('//a')
     for y in range(0, len(Links)):
-        if "forcedownload" in Links[y].get("href") and "keane_kelly_daniel" not in Links[y].get("href"):
+        if "forcedownload" in str(Links[y].get("href")) and "keane_kelly_daniel" not in str(Links[y].get("href")):
             data += ("<p><a href='" + Links[y].get("href").replace("?forcedownload=1", "") + "' style='color: #212121;'>" + Links[y].text  + "</a></p>")
             #print (Links[y].text.replace("Job Spec -","").replace("Job Spec ","").replace(".pdf","") +  "   " + Links[y].get("href").replace("?forcedownload=1", ""))
     return data
